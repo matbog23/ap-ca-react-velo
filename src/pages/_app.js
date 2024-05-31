f291a8342f1b5559df44fbddb36a563e3e91a6c1
Mathieu Boogaerts <Booga3rtsM@gmail.com>
2024-05-31 19:51:01 +0200

    Fix: prebuild push

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import "../styles/globals.css"; // Import other global styles if any
 
 import Layout from "@/components/Layout";
 
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
